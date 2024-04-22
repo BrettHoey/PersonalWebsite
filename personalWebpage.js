cc143bd2039c5115d353b5ee2b13c5158156d1b0
BrettHoey <150977354+BrettHoey@users.noreply.github.com>
2024-04-22 16:11:53 -0500

    Add files via upload

---
+++ personalWebpage.js
@@ -0,0 +1,11 @@
+function getUserInput()
+{
+    var x = document.getElementById("userText").value;
+    document.getElementById("h4").innerText = "your output is:" + x; 
+
+}
+
+function goToGame()
+{
+    window.location.href = "minigame41824.html";
+}
